794f35f4c54055407cc63490471140574c3cb82d
Raphael Rivas <raphaelrivas@hotmail.com>
2023-04-03 01:49:09 -0300

    refactor: working compiler as class

--- assembly_interpreter.py
+++ assembly_interpreter.py
@@ -3,12 +3,142 @@ Este interpretador emula um processador hipotético.
 
 Requisitos
     - CR armazena comparação
-    - O interpretador deve avisar erro léxico, sintático ou semântico
-"""
+    - O interpretador deve avisar erro léxico, sintático ou semântico"""
 
-import ctypes
+class MiniAssembler:
+    "Class for simple Assembly compiler and runner"
+    def __init__(self):
+        self.registers = {'CP': 0}
+        self.labels = {}
+        self.memory = [0] * 15 + [1.2]
+        self.pc = 0
+        self.instructions = []
 
-INPUT = """
+    def __str__(self) -> str:
+        return f"""\
+Instructions:   {self.instructions}
+Memory:         {self.memory}
+Registers:      {self.registers}
+PC:             {self.pc}
+Labels:         {self.labels}\
+        """
+
+    def load(self, code):
+        self.instructions = []
+        self.labels = {}
+        for i, line in enumerate(code.split('\n')):
+            line = line.split("--")[0].strip()
+            if not line:
+                continue
+            if ':' in line:
+                label, line = line.split(':', 1)
+                label = label.strip()
+                if label in self.labels:
+                    raise ValueError(f"Duplicate label '{label}' on line {i+1}")
+                self.labels[label] = len(self.instructions)
+            self.instructions.append(line)
+
+    def run(self):
+        while self.pc < len(self.instructions):
+            instruction = self.instructions[self.pc]
+            operation, *args = instruction.split()
+            if operation == "HALT":
+                return
+            if operation in ["MOVE","ADD","SUBT","JUMP","JTRUE","JFALSE","CMP","CMAIOR", "CMENOR",
+                "MULT", "DIV", "VAR", "INT"]:
+                getattr(self, operation.lower())(*[i.strip(',') for i in args])
+            else:
+                raise Exception(f'Unknown instruction: {instruction}')
+            self.pc += 1
+
+    def operator_is_register(self, src):
+        return src in self.registers
+
+    def operator_is_label(self, src):
+        return src in self.labels and self.labels[src] in self.memory
+
+    def operator_is_value(self, src):
+        return not self.operator_is_register(src) and not self.operator_is_label(src)
+
+    def get_operator(self, src):
+        "Given source, get operator's value from registers or labels"
+        # register
+        if self.operator_is_register(src):
+            return self.registers[src]
+        # memory
+        elif self.operator_is_label(src):
+            return self.memory[self.labels[src]]
+        # value
+        else:
+            if src.isnumeric():
+                astype = int
+                if  '.' in src:
+                    astype = float
+                return astype(src)
+            return src
+
+    def set_operator(self, value, reference:str):
+        if reference[:0]+reference[:1] == "[]":
+            label = self.labels[reference[1:-1]]
+            self.memory[label] = value
+        else:
+            self.registers[reference] = value
+
+    def move(self, dst, src):
+        src_value = self.get_operator(src)
+        self.set_operator(src_value, dst)
+
+    def add(self, dst, src):
+        self.set_operator(self.get_operator(dst) + self.get_operator(src), dst)
+
+    def subt(self, dst, src):
+        self.set_operator(self.get_operator(dst) - self.get_operator(src), dst)
+
+    def mult(self, dst, src):
+        self.set_operator(self.get_operator(dst) * self.get_operator(src), dst)
+
+    def div(self, dst, src):
+        self.set_operator(self.get_operator(dst) / self.get_operator(src), dst)
+
+    def jump(self, dst:str):
+        if dst.isnumeric():
+            self.pc = int(dst) - 1
+        else:
+            self.pc = self.labels[dst] - 1
+
+    def jtrue(self, dst):
+        if self.registers['CP'] == 1:
+            self.jump(dst)
+
+    def jfalse(self, dst):
+        if self.registers['CP'] == 0:
+            self.jump(dst)
+
+    def cmp(self, op1, op2):
+        self.registers['CP'] = int(self.get_operator(op1) == self.get_operator(op2))
+
+    def cmaior(self, op1, op2):
+        self.registers['CP'] = int(self.get_operator(op1) > self.get_operator(op2))
+
+    def cmenor(self, op1, op2):
+        self.registers['CP'] = int(self.get_operator(op1) < self.get_operator(op2))
+
+    def var(self, label, address):
+        self.labels[label] = int(address)
+
+    def int(self, _type, address:str):
+        if not address.isnumeric():
+            return
+        if _type == "1":
+            self.memory[int(address)] = ord(input()[0])
+        if _type == "2":
+            print(chr(int(self.memory[int(address)])))
+
+
+MY_CODE = """\
+    VAR     teste 3
+    INT     1, 4
+    INT     2, 4
     MOVE    A, 6        -- Coloco o valor 6 no registrador A
     MOVE    B, 5        -- Coloco o valor 5 no registrador A
 enquanto:   MOVE  C, B  -- Coloco no registrador C o valor presente no registrador B
@@ -16,136 +146,14 @@ enquanto:   MOVE  C, B  -- Coloco no registrador C o valor presente no registrad
     JTRUE   fim         -- Se CR tiver o valor 1, então JUMP para a linha com o label fim
     MOVE    B, C        -- Coloco em B o valor presente em C
     MULT    A, B        -- Multiplico o valor presente em A com o valor presente em B. O resultado ficará em A
-    SUBT    B, 1        -- Subtraio 1 no o valor presente em B 
+    SUBT    B, 1        -- Subtraio 1 no o valor presente em B
     JUMP    enquanto    -- JUMP para linha com o label enquanto.
 fim:        HALT
 """
 
-class Asasembly
-
-class AssemblyCompiler:
-    """Class for Assembly compiler and runner"""
-
-    variables = {
-        'CR': -1,
-        'INT1': 0,
-        'INT2': 0,
-    }
-    mems = {}
-    labels = {}
-
-    def forrmat_input(self, x):
-        value = self.variables.get(x)
-        if value:
-            return value
-        elif x.isnumeric():
-            astype = int
-            if  '.' in x:
-                astype = float
-            return astype(x)
-
-    def move(self, x, y: str):
-        """Mnemonic for MOVE"""
-        y_value = self.forrmat_input(y)
-        self.variables.update({x: y_value})
-
-    def calc(self, x, y: str, op: str):
-        """Mneminic for MOVE"""
-        y_value = self.forrmat_input(y)
-        if not isinstance(y_value, (int, float)):
-            print("calc type is not number:", y, y_value, type(y_value))
-            exit(1)
-        if op == "*":
-            calc_result = self.variables[x] * y_value
-        elif op == "+":
-            calc_result = self.variables[x] + y_value
-        elif op == "-":
-            calc_result = self.variables[x] - y_value
-        else:
-            print("invalid operation")
-            exit(1)
-        self.variables.update({x: calc_result})
-
-    def comp(x,y, op):
-        """Compare with operator"""
-        if op == ">":
-            return x > y
-        elif op == "<":
-            return x < y
-        elif op == "=":
-            return x == y
-
-    def halt():
-        """Print variables and exit."""
-        print(f"""
-    Result:
-    {variables}
-        """)
-
-    def set_char_to_mem(self, address):
-        """Read key and set to memory address"""
-        ctypes.memmove(address, input()[0].encode('ascii'), 1)
-
-    def get_char_to_mem(self, address):
-        """Read key and set to memory address"""
-        print(ctypes.c_char.from_address(address).value, end='')
-
-    MNEMONICS = {
-        "MOVE": move,
-        "CMP": lambda x, y: variables.update({"CR": variables[x] == int(y)}),
-        "CMAIOR": lambda x,y: comp(x,y,">"),
-        "CMENOR": lambda x,y: comp(x,y,"<"),
-        "ADD": lambda x,y: calc(x,y, "+"),
-        "MULT": lambda x,y: calc(x,y, "*"),
-        "SUBT": lambda x, y: calc(x,y, "-"),
-        "JUMP": lambda x: labels[x] if labels.get(x) else -1,
-        "JTRUE": lambda x: labels[x] if variables['CR'] == 1 else False,
-        "JFALSE": lambda x: labels[x] if variables['CR'] != 1 else False,
-        "HALT": halt,
-        "INT 1": set_char_to_mem,
-        "INT 2": get_char_to_mem,
-    }
-
-    def interpret(INPUT):
-        lines = INPUT.split('\n')
-
-        # map labels
-        line_index = 0
-        for l, line in enumerate(lines):
-            line_splitted = line.replace(",", "").split("--")[0].split()
-            if not line_splitted:
-                line_index += 1
-                continue
-            if line_splitted[0].endswith(":"):
-                labels[line_splitted[0][:-1]] = l
-                line_splitted = line_splitted[1:]
-
-
-        # run code
-        line_index = 0
-        while line_index < len(lines):
-            line_splitted = lines[line_index].replace(",", "").split("--")[0].split()
-            if not line_splitted:
-                line_index += 1
-                continue
-            if line_splitted[0].endswith(":"):
-                if len(line_splitted) == 1:
-                    line_index += 1
-                    continue
-                line_splitted = line_splitted[1:]
-
-            # Call instruction
-            instruction = line_splitted[0]
-
-            # jump
-            result = MNEMONICS[instruction](*line_splitted[1:])
-            if instruction in ["JUMP", "JTRUE", "JFALSE"] and result is not False:
-                line_index = result
-                continue
-            if instruction == "HALT":
-                exit(0)
-
-            line_index += 1
-
-    result = interpret(INPUT)
-    print(result)
+assembler = MiniAssembler()
+assembler.load(MY_CODE)
+print(assembler.registers)
+assembler.run()
+# print(assembler)
+print(assembler.registers) # {'A': 12, 'B': 10}
